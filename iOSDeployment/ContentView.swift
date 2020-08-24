47e11acec8d20fb804f8a553e6329f60c2682919
Hardeep Singh <hsingh@servicetitan.com>
2020-08-24 14:20:31 -0700

    Initial Commit

---
+++ iOSDeployment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSDeployment
+//
+//  Created by Hardeep Singh on 8/24/20.
+//  Copyright © 2020 Hardeep Singh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
